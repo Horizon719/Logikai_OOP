1578817ace1fed513923db3fc51fa30b23d1ba08
PetiDominik <petidomi05@gmail.com>
2024-01-08 13:59:18 +0100

    Allitas oszály bevezetése

--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -4,11 +4,14 @@ package logikaioop;
 public class LogikaiOOP {
 
     public static void main(String[] args) {
+        Allitas a1 = new Allitas("Az 1. állítás!");
+        Allitas a2 = new Allitas("A 2. állítás!");
+        Allitas a3 = new Allitas("A 3. állítás!");
         
         System.out.println("Leírás");
-        System.out.println("Az 1. állítás!");
-        System.out.println("A 2. állítás!");
-        System.out.println("A 3. állítás!");
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
         System.out.println("Melyik:");
         System.out.println("A válasz: jó | rossz");
         
