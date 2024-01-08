9ea9d5eea8c06b7d72a050012575daaf196a0d3d
Horizon719 <milichovszky.sandor@gmail.com>
2024-01-08 14:01:12 +0100

    Allitas osztály

---
+++ src/logikaioop/Allitas.java
@@ -0,0 +1,14 @@
+package logikaioop;
+
+public class Allitas {
+    private String allitas;
+
+    public Allitas(String allitas) {
+        this.allitas = allitas;
+    }
+
+    @Override
+    public String toString() {
+        return allitas;
+    }
+}
--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -5,10 +5,14 @@ public class LogikaiOOP {
 
     public static void main(String[] args) {
         
+        Allitas allitas1 = new Allitas("Az 1. állítás!");
+        Allitas allitas2 = new Allitas("A 2. állítás!");
+        Allitas allitas3 = new Allitas("A 3. állítás!");
+        
         System.out.println("Leírás");
-        System.out.println("Az 1. állítás!");
-        System.out.println("A 2. állítás!");
-        System.out.println("A 3. állítás!");
+        System.out.println(allitas1);
+        System.out.println(allitas2);
+        System.out.println(allitas3);
         System.out.println("Melyik:");
         System.out.println("A válasz: jó | rossz");
         
