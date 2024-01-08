374e4d61825e5464f668b4d9c99c1583b6d181cd
PetiDominik <petidomi05@gmail.com>
2024-01-08 15:20:25 +0100

    feladatok felbontasa, 1, 2

---
+++ src/logikaioop/Program.java
@@ -0,0 +1,34 @@
+
+package logikaioop;
+
+import java.util.Scanner;
+
+public class Program {
+    private static final Scanner SC = new Scanner(System.in);
+    
+    public Program() {
+        this.elso();
+        int szam = this.masodik();
+    }
+    
+    private void elso() {
+        Lada a1 = new Lada("Az 1. állítás!", "fa", true);
+        Lada a2 = new Lada("A 2. állítás!", "kő", false);
+        Lada a3 = new Lada("A 3. állítás!", "gyémánt", true);
+        
+        System.out.println("Leírás");
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+    }
+    
+    private int masodik() {
+        int szam;
+        System.out.print("Melyik: ");
+        do {
+            szam = SC.nextInt();
+        } while (szam < 1 || szam > 3);
+        
+        return szam;
+    }
+}
