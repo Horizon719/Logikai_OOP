e6972ddfed67afeeb36bb8da9487dbb94defa49b
Horizon719 <milichovszky.sandor@gmail.com>
2024-01-08 15:21:58 +0100

    harmadik feladat

--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -7,18 +7,7 @@ public class LogikaiOOP {
     private static final Scanner SC = new Scanner(System.in);
     
     public static void main(String[] args) {
-        Lada a1 = new Lada("Az 1. állítás!", "fa", true);
-        Lada a2 = new Lada("A 2. állítás!", "kő", false);
-        Lada a3 = new Lada("A 3. állítás!", "gyémánt", true);
-        
-        System.out.println("Leírás");
-        System.out.println(a1);
-        System.out.println(a2);
-        System.out.println(a3);
-        System.out.print("Melyik: ");
-        int szam = SC.nextInt();
-        System.out.println("A válasz: jó | rossz");
-        
+        new Program();
     }
     
 }
---
+++ src/logikaioop/Program.java
@@ -0,0 +1,15 @@
+package logikaioop;
+
+public class Program {
+
+    public Program() {
+    }
+    
+    private void harmadik(int szam){
+        if (szam == 3){
+            System.out.println("A válasz helyes.");
+        } else{
+            System.out.println("A válasz helytelen.");
+        }
+    }
+}
