51e5b3ae7973c31ef5f249d01a3e42218c0d06a9
Horizon719 <milichovszky.sandor@gmail.com>
2024-01-15 14:27:40 +0100

    view létrehozása

---
+++ src/logikaioop/view/ConsoleView.java
@@ -0,0 +1,27 @@
+package logikaioop.view;
+
+import java.util.Scanner;
+
+public class ConsoleView {
+    private static final Scanner SC = new Scanner(System.in);
+    
+    public void kiir(String str){
+        System.out.println(str);
+    }
+    
+    public <T> void kiir(T[] obj){
+        for (T t : obj) {
+            this.kiir(t.toString());
+        }
+    }
+    
+    public int szamBekerTartomannyal(String szoveg, int min, int max){
+        int szam;
+        do {
+            System.out.print(szoveg);
+            szam = SC.nextInt();
+        } while (szam < min || szam > max);
+        
+        return szam;
+    }
+}
